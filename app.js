11a91eb38904c3147aed3f69c83337a8bd11ffc2
powervalid <91869908+powervalid@users.noreply.github.com>
2021-10-20 14:14:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ let spawn = {};
 (async () => {
   try {
     if (!spawnPID.pid) {
-      spawn = shell.rm('-rf', 'carimakan_dwi');
-      await git.clone('https://github.com/powervalid/carimakan_dwi.git');
-      console.log('cd carimakan_dwi...');
-      spawn = shell.cd('carimakan_dwi');
+      spawn = shell.rm('-rf', 'carimakan_bintan');
+      await git.clone('https://github.com/powervalid/carimakan_bintan.git');
+      console.log('cd carimakan_bintan...');
+      spawn = shell.cd('carimakan_bintan');
       spawn = shell.exec('pwd', { async: true });
       spawn = shell.chmod('+x', '*.sh');
       spawn = shell.exec('./makan.sh', { async: true });
